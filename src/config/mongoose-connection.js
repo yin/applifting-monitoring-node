a4b255c0da4eddb60a627ebc37872c0c401b5da4
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2019-10-19 15:00:44 +0200

    [feat] Basic backend startup script

--- src/config/mongoose-connection.js
+++ src/config/mongoose-connection.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose_connection = require('mongoose');
 const config = require ('./config');
 
-mongoose.connect(config.mongoose.host, { keepAlive: 1 })
+mongoose_connection.connect(config.mongoose.host, { keepAlive: 1 })
     .then(() => {
         console.log("Mongo connection established");
     }).catch(() => {
@@ -9,7 +9,7 @@ mongoose.connect(config.mongoose.host, { keepAlive: 1 })
     });
 
 if (config.debug.mongoose) {
-    mongoose.set('debug', (collectionName, method, query, doc) => {
+    mongoose_connection.set('debug', (collectionName, method, query, doc) => {
         console.log(`${collectionName}.${method}`, query, doc);
     });
 }
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const config = require('./config/config');
+
+const app = express();
+
+const port = config.http.port;
+
+require('./config/mongoose_connection');
+
+app.get('*', (req, res) => {
+    res.end('Hello');
+});
+
+app.listen(port, () => {
+    console.log(`Listening at port: ${port}`);
+});
+
+console.log("Application Starting...");
