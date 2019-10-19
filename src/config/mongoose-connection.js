65aa855b6973d9c9d6959828079d258a45da5929
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2019-10-19 19:43:01 +0200

    [code] mongoose-connection.js new mongo url parser

--- src/config/mongoose-connection.js
+++ src/config/mongoose-connection.js
@@ -1,10 +1,13 @@
 const mongooseConnection = require('mongoose');
-const config = require ('./config');
+const config = require('./config');
 
-mongooseConnection.connect(config.mongoose.host, { keepAlive: 1 })
+mongooseConnection.connect(config.mongoose.host, { keepAlive: 1, useNewUrlParser: true })
     .then(() => {
         console.log("Mongo connection established");
-    }).catch(() => {
+    })
+    .catch(() => {
+        // TODO yin: This causes unhandled promise rejection. Handle startup errors comprehensively.
+        //      Export the promise from the module.
         throw new Error('Unable to connect to database');
     });
 
