72be5ab183b511f1e44828bdd1d14a34ecc54e50
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2019-10-19 19:43:55 +0200

    fixup! [feat] Mongoose User schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,25 +3,24 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     userName : {
         type: String,
+        index: { unique: true },
         required: true,
     },
     email: {
         type: String,
-        validate: {
-            // TODO yin: RFC validation specs
-            validator: (email) => /^([\w-\.]+@([\w-]+\.)+[\w-])?$/.test(email.text),
-            message: (email) => `String '${email}' is not a valid email`
-        },
+        index: { unique: true },
+        required: true,
+        validate: /^[\w-\.]+@([\w-]+\.)*[\w-]+$/,
     },
     accessToken: {
         type: String,
-        validate: {
-            // TODO yin: RFC validation specs
-            validator: (token) =>
-                /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/.test(token),
-            message: (token) => `Invalid access-token: '${token}'`
-        }
+        index: {
+            unique: true,
+            partialFilterExpression: { accessToken: { $exists: true } },
+        },
+        // TODO yin: RFC validation specs
+        validate: /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/,
     },
 });
 
-module.exports = mongoose.Model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
