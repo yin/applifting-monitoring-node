d13656ad95620fe7d04745f9e94a645f0a6215d3
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2019-10-19 19:44:34 +0200

    fixup! [feat] Added body-parser for parsing structured data requests

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: 'text/plain' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -21,6 +22,10 @@ app.use(function(req, res, next) {
 
 app.get('*', (req, res) => {
     res.status(400).end('No content');
+});
+
+app.use((err, req, res, next) => {
+    res.status(400).send({error: err});
 });
 
 app.listen(port, () => {
