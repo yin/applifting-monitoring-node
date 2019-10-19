0fa183311d37399af279960830a5dec81305c9fa
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2019-10-19 19:46:22 +0200

    [feat] User model API endpoint

---
+++ src/api/user.route.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const User = require('../models/user.model');
+
+router.get('/:id/action/:action', function (req, res) {
+    res.end(":P");
+});
+
+// REST List
+router.get('/', function (req, res) {
+    User.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            console.log('Unable to load users', error);
+            res.status(500).json({ error: error, message: 'Unable to load users' });
+        });
+});
+
+// REST Read
+router.get('/:id', function (req, res) {
+    User.findById(req.params.id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            console.log(`Unable to load user id=${req.params.id}`, error);
+            res.status(500).json({ error: error, message: `Unable to load user id=${req.params.id}` });
+        });
+});
+
+// REST Create
+router.post('/', function (req, res) {
+    const t = req.body;
+    delete t._id;
+    let user = new User(req.body);
+    user.save()
+        .then(newUser => {
+            res.status(200).json(newUser);
+        })
+        .catch(error => {
+            console.log('Unable to create new user', error);
+            res.status(500).json({ error: error, message: 'Unable to create new user' });
+        });
+});
+
+// REST Update
+router.put('/:id', function (req, res) {
+    let id = req.params.id;
+    User.findByIdAndUpdate({ _id: id }, req.body, { new: true })
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            console.error(`Unable to update user id=${req.params.id}`, error);
+            res.status(500).json({ error: error, message: `Unable to update user id=${req.params.id}` });
+        });
+});
+
+// REST Delete
+router.delete('/:id', function (req, res) {
+    User.findByIdAndDelete({ _id: req.params.id })
+        .then(user => {
+            return res.status(200).json(user);
+        })
+        .catch(error => {
+            console.error(`Unable to delete user id=${req.params.id}`, error);
+            res.status(500).json({ error: error, message: `Unable to delete user id=${req.params.id}` });
+        });
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -12,13 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: 'text/plain' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
     return next();
 });
+
+app.use('/user', require('./api/user.route'));
 
 app.get('*', (req, res) => {
     res.status(400).end('No content');
